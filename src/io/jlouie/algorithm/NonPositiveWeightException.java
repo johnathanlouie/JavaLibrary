4c1af5ecf0a12b979fb6ee455ab377909405b9e3
Jonathan Louie <johnlouie88@outlook.com>
2018-04-03 22:02:54 -0700

    Change author

--- src/io/jlouie/algorithm/NonPositiveWeightException.java
+++ src/io/jlouie/algorithm/NonPositiveWeightException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 jlouie
+ * Copyright (C) 2018 Johnathan Louie
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,7 +18,7 @@ package io.jlouie.algorithm;
 
 /**
  *
- * @author jlouie
+ * @author Johnathan Louie
  */
 public class NonPositiveWeightException extends RuntimeException {
 
